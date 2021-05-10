53e5e6909f49789ef4c484185fdb21a96bebc2a9
als10 <alston03.dmello@gmail.com>
2021-05-10 18:32:32 +0400

    Complete exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
 
 const App = () => {
   const [ persons, setPersons ] = useState([
@@ -46,34 +50,22 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          filter shown with<input
-                              value={filter}
-                              onChange={handleFilterChange}
-                           />
-        </div>
-      </form>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input
-                  value={newName}
-                  onChange={handleNameChange}
-                />
-        </div>
-        <div>
-          number: <input 
-                    value={newNumber}
-                    onChange={handleNumberChange}
-                  />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {personsToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm 
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToShow} />
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <form>
+        <div>
+          filter shown with<input
+                              value={filter}
+                              onChange={handleFilterChange}
+                            />
+        </div>
+    </form>
+  )
+}
+
+export default Filter
