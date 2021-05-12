53528f8ca2e7193cdc7bf6fd3ccf004d70c176ba
als10 <alston03.dmello@gmail.com>
2021-05-12 20:59:04 +0400

    Complete exercise 3.15

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -14,29 +14,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-    {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-    },
-    {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-    },
-    {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-    },
-    {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-    }   
-]
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
       response.json(persons)
@@ -64,9 +41,11 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
+    Person
+        .findByIdAndRemove(request.params.id)
+        .then(result =>
+            response.status(204).end()
+        )
 })
 
 app.post('/api/persons', (request, response) => {
