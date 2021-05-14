efe4d83e7400f565e2c9eff561c59c4277973cb9
als10 <alston03.dmello@gmail.com>
2021-05-14 13:25:02 +0400

    Complete exercise 4.8

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -12,5 +13,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
