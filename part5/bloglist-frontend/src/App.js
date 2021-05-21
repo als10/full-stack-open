e2d1808b9302a67bf02af75d0ea1c106bfcb49ae
als10 <alston03.dmello@gmail.com>
2021-05-21 19:10:31 +0400

    Complete exercise 5.5

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import Blog from './components/Blog'
 import BlogForm from './components/BlogForm'
 import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -12,6 +13,12 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [message, setMessage] = useState({})
+  const [blogFormVisible, setBlogFormVisible] = useState(false)
+
+  const hideWhenVisible = { display: blogFormVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogFormVisible ? '' : 'none' }
+
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -56,6 +63,7 @@ const App = () => {
   }
 
   const addBlog = async (blog) => {
+    blogFormRef.current.toggleVisibility()
     try {
       const newBlog = await blogService.create(blog)
       setBlogs(blogs.concat(newBlog))
@@ -65,22 +73,17 @@ const App = () => {
     }
   }
 
+  const blogForm = () => (
+    <Togglable buttonLabel='new blog' ref={blogFormRef}>
+        <BlogForm createBlog={addBlog} />
+    </Togglable>
+  )
+
   const blogsList = () => (
     <div>
-      <h2>blogs</h2>
-      <Notification message={message} />
-      <div>
-        {user.name} logged in
-        <button onClick={logout}>logout</button>
-      </div>
-      <br />
-      <BlogForm createBlog={addBlog} />
-      <br />
-      <div>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
-      </div>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
     </div>
   )
 
@@ -92,7 +95,17 @@ const App = () => {
           <Notification message={message} />
           <LoginForm handleLogin={handleLogin} message={message}/>
         </div>) :
-        blogsList()}
+        (<div>
+          <h2>blogs</h2>
+          <Notification message={message} />
+          <div>
+            {user.name} logged in
+            <button onClick={logout}>logout</button>
+          </div>
+          <br />
+          {blogForm()}
+          {blogsList()}
+        </div>)}
     </>
   )
 }
